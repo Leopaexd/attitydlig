98e6c715811b1d4ba23f34efca04caa59eeed000
Oliver <oliver.glant@gmail.com>
2018-03-14 16:19:30 +0100

    First implementation

--- dictionary.py
+++ dictionary.py
@@ -2,5 +2,22 @@
 # Attitydlig - attitydanalys på svenska
 # Lexikon
 
-class Dictionary:
-    pass
+
+import time
+
+
+class Dictionary(object):
+
+    def __init__(self, post_array):
+        self.dictionary = dict()
+        start = time.time()
+        print("Indexing words")
+        for post in post_array:
+            for word in post:
+                if word not in self.dictionary:
+                    self.dictionary[word] = len(self.dictionary)
+        time_elapsed = time.time() - start
+        print("Indexing completed in ", ("%.2f" % time_elapsed), "seconds")
+
+    def clear(self):
+        self.dictionary.clear()
