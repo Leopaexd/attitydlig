18e05e1b4e5acf6b8cec6dfbcdf490b4c5622fbc
Oliver <oliver.glant@gmail.com>
2018-03-14 16:30:41 +0100

    Implemented a simple vectorizer, for use in trial runs

--- attitydlig.py
+++ attitydlig.py
@@ -12,7 +12,6 @@ def main():
     extracted_data = extractor.extract('inputfile')
     preprocessed_data = preprocessor.preprocess(extracted_data)
     dictionary = dict.Dictionary(preprocessed_data)
-    vectorized_data = vectorizer.vectorize_data(preprocessed_data,dictionary)
-    pass
+    vectorized_data = vectorizer.vectorize_data(preprocessed_data,dictionary,300) # 300 word maximum length
 
 main()
--- vectorizer.py
+++ vectorizer.py
@@ -5,12 +5,19 @@
 import numpy as np
 
 # Return the vector representation of a review, using a specified dictionary.
-def vectorize_review(review,dictionary):
-    return vector
+# First version - simple vector representation including word order but having a maximum length.
+def vectorize_review(review,dictionary,length):
+    vector = []
+    for word in review:
+        vector.append(dictionary.dictionary[word])
+    # Add padding until vector is of specified length
+    while len(vector) < length:
+        vector.append(0)
+    return np.asarray(vector)
 
 # Vectorize a list of reviews, and return a numpy 2D matrix of the stacked vectors
-def vectorize_data(preprocessed_data,dictionary):
+def vectorize_data(preprocessed_data,dictionary, length):
     vectorized_data = []
     for review in preprocessed_data:
-        vectorized_data.append(vectorize_review(review,dictionary))
+        vectorized_data.append(vectorize_review(review,dictionary, length))
     return np.asarray(vectorized_data)
