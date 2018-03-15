8b2b6b6e4d825c0794793abcb02a99fa2f1f65b6
Oliver <oliver.glant@gmail.com>
2018-03-15 16:06:28 +0100

    Added file extraction. Using English reviews for trial runs.

--- attitydlig.py
+++ attitydlig.py
@@ -9,7 +9,12 @@ import preprocessor
 import vectorizer
 
 def main():
-    extracted_data = extractor.extract('inputfile')
+    directory = 'C:\\Users\\olive\\Desktop\\Datamängder för uppsats\\ESCW 2016'
+
+    extracted_data = extractor.extract(directory)
+
+    for line in extracted_data:
+        print(line)
     preprocessed_data = preprocessor.preprocess(extracted_data)
     dictionary = dict.Dictionary(preprocessed_data)
     vectorized_data = vectorizer.vectorize_data(preprocessed_data,dictionary,300) # 300 word maximum length
--- extractor.py
+++ extractor.py
@@ -2,7 +2,16 @@
 # Attitydlig - attitydanalys på svenska
 # Extrahering
 
-# Extract reviews from file and return them in a list.
-def extract(raw_file):
+import os
+
+# Extract reviews from files in directory and return them in a list.
+def extract(directory):
     reviews = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            with open(os.path.join(root, file), "r") as review_file:
+                for line in review_file.readlines():
+                    reviews.append(line)
+
     return reviews
+
