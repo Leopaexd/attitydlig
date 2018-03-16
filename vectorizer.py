f5eed33396764390931efeaa0c6a36b8ada58e3c
Oliver <oliver.glant@gmail.com>
2018-03-16 13:08:28 +0100

    Added timer

--- vectorizer.py
+++ vectorizer.py
@@ -3,6 +3,7 @@
 # Vektorisering
 
 import numpy as np
+import time
 
 # Return the vector representation of a review, using a specified dictionary.
 # First version - simple vector representation including word order but having a maximum length.
@@ -17,7 +18,11 @@ def vectorize_review(review,dictionary,length):
 
 # Vectorize a list of reviews, and return a numpy 2D matrix of the stacked vectors
 def vectorize_data(preprocessed_data,dictionary, length):
+    start = time.time()
+    print('Vectorizing...')
     vectorized_data = []
     for review in preprocessed_data:
         vectorized_data.append(vectorize_review(review,dictionary, length))
+    time_elapsed = time.time() - start
+    print("Vectorization completed in ", ("%.2f" % time_elapsed), "seconds")
     return np.asarray(vectorized_data)
