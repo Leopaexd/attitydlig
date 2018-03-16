58ad98bf6fd0ab12d5cdcbdaa56bbddb2c6188b6
Oliver <oliver.glant@gmail.com>
2018-03-16 12:27:16 +0100

    Tokenizes, stems and removes some interpunctuation

--- preprocessor.py
+++ preprocessor.py
@@ -2,16 +2,21 @@
 # Attitydlig - attitydanalys på svenska
 # Förprocessning
 
+from nltk.stem.snowball import SnowballStemmer
+from nltk.tokenize import word_tokenize
 
 # Take a list of unprocessed reviews and return a list of preprocessed reviews.
 def preprocess(unprocessed_reviews):
+    stemmer = SnowballStemmer('english')
     preprocessed_reviews = []
     for review in unprocessed_reviews:
-        tokenized_review = tokenize(review)
+        tokenized_review = []
+        for word in word_tokenize(review):
+            if word not in ["." , ","]:
+                tokenized_review.append(stemmer.stem(word).upper())
+
         preprocessed_reviews.append(tokenized_review)
+        print(tokenized_review)
 
 
     return preprocessed_reviews
-
-def tokenize(review):
-    return review.split()
