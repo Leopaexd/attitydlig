93e2e281a46a5c0a7caccdf1f92f98d3a70b3278
Oliver <oliver.glant@gmail.com>
2018-03-16 12:57:32 +0100

    Now prints time and number of reviews preprocessed

--- preprocessor.py
+++ preprocessor.py
@@ -4,19 +4,22 @@
 
 from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import word_tokenize
+import time
 
 # Take a list of unprocessed reviews and return a list of preprocessed reviews.
 def preprocess(unprocessed_reviews):
+    start = time.time()
     stemmer = SnowballStemmer('english')
     preprocessed_reviews = []
+    print ("Preprocessing...")
+    number_of_reviews = 0
     for review in unprocessed_reviews:
         tokenized_review = []
         for word in word_tokenize(review):
             if word not in ["." , ","]:
                 tokenized_review.append(stemmer.stem(word).upper())
-
         preprocessed_reviews.append(tokenized_review)
-        print(tokenized_review)
-
-
+        number_of_reviews += 1
+    time_elapsed = time.time() - start
+    print("Preprocessed " + str(number_of_reviews) + "reviews in ", ("%.2f" % time_elapsed), "seconds")
     return preprocessed_reviews
