fd86bf5fc93c328ba3989d8b1b2c683a8f28c1ac
Oliver <oliver.glant@gmail.com>
2018-03-16 12:03:11 +0100

    Added tokenization

--- preprocessor.py
+++ preprocessor.py
@@ -2,6 +2,16 @@
 # Attitydlig - attitydanalys på svenska
 # Förprocessning
 
+
 # Take a list of unprocessed reviews and return a list of preprocessed reviews.
-def preprocess(unprocessed_data):
-    return preprocessed_data
+def preprocess(unprocessed_reviews):
+    preprocessed_reviews = []
+    for review in unprocessed_reviews:
+        tokenized_review = tokenize(review)
+        preprocessed_reviews.append(tokenized_review)
+
+
+    return preprocessed_reviews
+
+def tokenize(review):
+    return review.split()
