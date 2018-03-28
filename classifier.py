6b18cf58a971b1e6a46695fd5a086ff7ac3fb537
Oliver <oliver.glant@gmail.com>
2018-03-28 10:13:55 +0200

    Implemented a Keras classifier

--- classifier.py
+++ classifier.py
@@ -3,13 +3,39 @@
 # Klassificering
 
 
-from tpot import TPOTClassifier
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+import time
 
 class Classifier:
     def __init__(self):
-        self.tpot = TPOTClassifier(generations=1000, population_size=20, verbosity=3, max_eval_time_mins=3,
-                                   periodic_checkpoint_folder='my_checkpoints')
+        self.model = Sequential()
+        self.model.add(Dense(units=128, activation='relu',input_dim=300))
+        self.model.add(Dense(units=2, activation='softmax'))
+        self.model.compile(loss='categorical_crossentropy',
+                           optimizer='sgd',
+                           metrics=['accuracy'])
 
-    def fit(self,reviews,polarities):
-        self.tpot.fit(reviews, polarities)
-        self.tpot.export('tpot_exported_pipeline.py')
+    def fit(self, x_training, y_training):
+        start = time.time()
+        self.model.fit(x_training, y_training, epochs=2000, batch_size=64, verbose=0,validation_split=0.1)
+        time_elapsed = time.time() - start
+        print("Model fit in ", ("%.2f" % time_elapsed), "seconds")
+
+    def evaluate(self,x_testing,y_testing):
+        loss_and_metrics = self.model.evaluate(x_testing,y_testing,batch_size=128,verbose=1)
+        print(loss_and_metrics)
+
+    def custom_evaluate(self,x_testing,y_testing):
+        predictions = self.model.predict(x_testing,verbose=1)
+        hits = 0
+        total = len(y_testing)
+        if len(y_testing) != len(predictions):
+            print('Length mismatch')
+        for a,b in zip(predictions,y_testing):
+            if np.all(a == b): hits += 1
+        print('Accuracy: ' + str(hits/total) + '. ' + str(hits) + '/' + str(total) + ' hits')
+        print('x_testing: ' +str(len(x_testing)) + ' y_testing: ' + str(len(y_testing)))
+        loss_and_metrics = self.model.evaluate(x_testing, y_testing, batch_size=128, verbose=1)
+        print(loss_and_metrics)
