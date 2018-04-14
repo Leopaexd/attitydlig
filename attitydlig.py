eac4fdc980266f4960f22b3e5fde35bb1513c21b
Oliver <oliver.glant@gmail.com>
2018-04-14 15:38:17 +0200

    Implementing Hyperas

--- attitydlig.py
+++ attitydlig.py
@@ -2,7 +2,7 @@
 # Attitydlig - attitydanalys på svenska
 # Huvudfil
 
-import new_classifier as classifier
+import classifier as classifier
 import dictionary as dict
 import extractor
 import preprocessor
@@ -10,58 +10,73 @@ import vectorizer
 import numpy as np
 import gensim
 import review_translator
+from hyperas import optim
+from hyperopt import Trials, STATUS_OK, tpe
 
-def main():
+
+def create_embedding_matrix(dictionary):
     word_vectors = gensim.models.KeyedVectors.load_word2vec_format('C:\\Users\\olive\\Desktop\\'
                                                                    'Datamängder för uppsats\\'
                                                                    'Swedish Word Vectors\\'
                                                                    'swectors-300dim.txt', binary=True,
                                                                    unicode_errors='ignore')
+    embedding_matrix = np.zeros((len(dictionary) + 1, 300))
+    for word, index in dictionary.items():
+        try:
+            word_vector = word_vectors[word]
+        except KeyError:
+            word_vector = None
+        if word_vector is not None:
+            embedding_matrix[index] = word_vector
+    return embedding_matrix
 
-    directory = 'C:\\Users\\olive\\Desktop\\Datamängder för uppsats\\Prisjakt\\Utvärderingsdata'
-
-    extracted_data = extractor.json_extract(directory)
-    extracted_reviews = extracted_data[0]
-    polarities = extracted_data[1]
-
-    preprocessed_reviews = preprocessor.preprocess(extracted_reviews)
-
-
-    # review_translator.translate_reviews(preprocessed_reviews, polarities)
-
-    dictionary = dict.Dictionary(preprocessed_reviews).dictionary
-
-    vectorized_data = vectorizer.vectorize_data(preprocessed_reviews,dictionary,300) # 300 word maximum length
+def prepare_data(preprocessed_reviews, polarities, dictionary):
+    vectorized_data = vectorizer.vectorize_data(preprocessed_reviews, dictionary, 300)  # 300 word maximum length
     x_training = vectorized_data[0]
     x_testing = vectorized_data[1]
 
     # split into training and testing data
-    y_training_and_testing = np.array_split(np.asarray(polarities),[0,int(len(polarities)*0.9)])
-    y_training =  y_training_and_testing[1]
+    y_training_and_testing = np.array_split(np.asarray(polarities), [0, int(len(polarities) * 0.9)])
+    y_training = y_training_and_testing[1]
     y_testing = y_training_and_testing[2]
 
-    pos = 0
-    tot = 0
-    for value in y_training:
-        tot += 1
-        if value == 1:
-            pos += 1
-    print('pos: ' + str(pos) + '/' + str(tot) +' = ' + str(pos/tot))
+    return (x_training, y_training, x_testing, y_testing, create_embedding_matrix(dictionary))
 
-
-    # Translated data 
-    with open('untranslated_reviews validation combined.txt','r') as file:
-        untranslated_reviews = np.concatenate(vectorizer.vectorize_data(preprocessor.preprocess(file.readlines()),dictionary,300))
-    with open ('translated_polarities validation combined.txt','r') as file:
+def translated_data(dictionary):
+    # review_translator.translate_reviews(preprocessed_reviews, polarities)
+    with open('untranslated_reviews validation combined.txt', 'r') as file:
+        untranslated_reviews = np.concatenate(
+            vectorizer.vectorize_data(preprocessor.preprocess(file.readlines()), dictionary, 300))
+    with open('translated_polarities validation combined.txt', 'r') as file:
         translated_polarities = []
         for line in file:
             translated_polarities.append(int(line))
+    return [untranslated_reviews,np.array(translated_polarities)]
+
+def main():
+    directory = 'C:\\Users\\olive\\Desktop\\Datamängder för uppsats\\Prisjakt\\Utvärderingsdata'
+    extracted_data = extractor.json_extract(directory)
+    extracted_reviews = extracted_data[0]
+    polarities = extracted_data[1]
+    preprocessed_reviews = preprocessor.preprocess(extracted_reviews)
+    dictionary = dict.Dictionary(preprocessed_reviews).dictionary
+
+    data = prepare_data(preprocessed_reviews, polarities, dictionary)
+    test_data = translated_data(dictionary)
+
+    best_run, best_model = optim.minimize(model=classifier.create_model,
+                              data=data,
+                              algo=tpe.suggest,
+                              max_evals=10,
+                              trials=Trials())
+    print("Evalutation of best performing model:")
+    print(best_model.evaluate(test_data[0],test_data[1]))
+    print("Best performing model chosen hyper-parameters:")
+    print(best_run)
+
+    # NN_classifier = classifier.create_model(data[0],data[1],data[2],data[3],data[4])
+    # print(NN_classifier.evaluate(test_data[0],test_data[1]))
 
 
-    NN_classifier = classifier.Classifier(dictionary,word_vectors)
-    NN_classifier.fit(x_training, y_training)
-    NN_classifier.evaluate(x_testing, y_testing)
-    NN_classifier.evaluate(untranslated_reviews, np.array(translated_polarities))
-    # NN_classifier.custom_evaluate(untranslated_reviews, translated_polarities)
 
 main()
--- classifier.py
+++ classifier.py
@@ -8,33 +8,37 @@ from keras.layers import *
 from keras import regularizers
 import numpy as np
 import time
+from hyperas.distributions import uniform, choice
+from hyperopt import STATUS_OK
 
-def create_model(x_training, y_training, x_testing, y_testing):
+def create_model(x_training, y_training, x_testing, y_testing, embedding_matrix):
     start = time.time()
     model = Sequential()
-    model.add(Embedding(len(dictionary) + 1,
-                             output_dim=EMBEDDING_DIM,
-                             weights=[self.embedding_matrix],
-                             input_length=MAX_SEQUENCE_LENGTH,
+    model.add(Embedding(np.shape(embedding_matrix)[0],
+                             output_dim=300,
+                             weights=[embedding_matrix],
+                             input_length=300,
                              trainable=True))
-    model.add(Conv1D(filters=100, kernel_size=3,
-                          padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+    model.add(Conv1D(filters={{choice([64,100,200])}}, kernel_size=3,
+                          padding='same', activation='relu', kernel_regularizer={{choice([none,regularizers.l2(0.01)])}}))
     model.add(MaxPooling1D(pool_size=2))
-    model.add(Conv1D(filters=100, kernel_size=4,
-                          padding='same', activation='relu'))
+    model.add(Conv1D(filters={{choice([64,100,200])}}, kernel_size=4,
+                          padding='same', activation='relu', kernel_regularizer={{choice([none,regularizers.l2(0.01)])}}))
+    model.add(MaxPooling1D(pool_size={{choice([2,3])}}))
+    model.add(Conv1D(filters={{choice([64,100,200])}}, kernel_size=5,
+                          padding='same', activation='relu', kernel_regularizer={{choice([none,regularizers.l2(0.01)])}}))
     model.add(MaxPooling1D(pool_size=2))
-    model.add(Conv1D(filters=100, kernel_size=5,
-                          padding='same', activation='relu'))
-    model.add(MaxPooling1D(pool_size=2))
-    model.add(Dropout(0.5))
+    model.add(Dropout({{uniform(0,0.6)}}))
     model.add(Flatten())
-    model.add(Dense(units=250, activation='relu'))
+    model.add(Dense(units=250, activation={{choice(['relu','sigmoid'])}}))
     model.add(Dense(units=1, activation='sigmoid'))
 
     model.compile(loss='binary_crossentropy', optimizer='adadelta',
                        metrics=['binary_accuracy'])
 
-    model.fit(x_training, y_training, epochs=5, batch_size=64, verbose=1, validation_split=0.1)
+    model.fit(x_training, y_training, epochs=5, batch_size={{choice([16, 32, 64, 128])}}, verbose=1, validation_split=0.1)
     time_elapsed = time.time() - start
     print("Model fit in ", ("%.2f" % time_elapsed), "seconds")
-    return model
+    score = model.evaluate(x_test, y_test, verbose=0)
+    accuracy = score[1]
+    return {'loss': -accuracy, 'status': STATUS_OK, 'model': model}
